f3d50d890d67a30450193f732a9531effe655fbc
robso <robso@ROBSON>
2024-10-01 19:41:36 -0300

    Add project files.

---
+++ Learn_N-TierArchitecture/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Learn_N_TierArchitecture.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
